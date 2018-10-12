6d88df6a90565d3a96c39eb3eac38b87fec9cb85
vlurgio <13vlurgio@gmail.com>
2018-10-12 14:08:57 -0400

    fixing index issues

--- seasonal_esd.py
+++ seasonal_esd.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats as stats
 import statsmodels.api as sm
 
+
 def calculate_test_statistic(ts):
     """Calculate the test statistic defined by being
        the top zscore in the timeseries.
@@ -13,9 +14,13 @@ def calculate_test_statistic(ts):
         tuple(int, float): The index of the top zscore and the value of the top zscore.
 
     """
-    zscores = abs(stats.zscore(ts, ddof=1))
+
+    nts = np.array([x[0] for x in ts])
+    print(nts)
+    zscores = abs(stats.zscore(nts, ddof=1))
     max_idx = np.argmax(zscores)
-    return max_idx, zscores[max_idx]
+    return zscores[max_idx], max_idx
+
 
 def calculate_critical_value(ts, alpha):
     """Calculate the critical value with the formula given for example in
@@ -29,15 +34,20 @@ def calculate_critical_value(ts, alpha):
         float: The critical value for this test.
 
     """
-    size   = len(ts)
+    size = len(ts)
     t_dist = stats.t.ppf(1 - alpha / (2 * size), size - 2)
-    
-    numerator   = (size - 1) * t_dist
-    denominator = np.sqrt(size ** 2 - size * 2 + size * t_dist ** 2)
+
+    numerator = (size - 1) * t_dist
+    denominator = np.sqrt(size**2 - size * 2 + size * t_dist**2)
 
     return numerator / denominator
 
-def seasonal_esd(ts, seasonality=None, hybrid=False, max_anomalies=10, alpha=0.05):
+
+def seasonal_esd(ts,
+                 seasonality=None,
+                 hybrid=False,
+                 max_anomalies=10,
+                 alpha=0.05):
     """Compute the Seasonal Extreme Studentized Deviate of a time series. 
        The steps taken are first to to decompose the timeseries into STL 
        decomposition (trend, seasonality, residual). Then, calculate 
@@ -62,15 +72,18 @@ def seasonal_esd(ts, seasonality=None, hybrid=False, max_anomalies=10, alpha=0.0
 
     """
     ts = np.array(ts)
-    seasonal = seasonality or int(0.2 * len(ts)) # Seasonality is 20% of the ts if not given.
-    decomp   = sm.tsa.seasonal_decompose(ts, freq=seasonal)
+    seasonal = seasonality or int(
+        0.2 * len(ts))  # Seasonality is 20% of the ts if not given.
+    decomp = sm.tsa.seasonal_decompose(ts, freq=seasonal)
     if hybrid:
-        mad      = np.median(np.abs(ts - np.median(ts)))
+        mad = np.median(np.abs(ts - np.median(ts)))
         residual = ts - decomp.seasonal - mad
     else:
         residual = ts - decomp.seasonal - np.median(ts)
     outliers = esd(residual, max_anomalies=max_anomalies, alpha=alpha)
-    return outliers
+    # embed()
+    return outliers, decomp
+
 
 def esd(timeseries, max_anomalies=10, alpha=0.05):
     """Compute the Extreme Studentized Deviate of a time series. 
@@ -87,17 +100,17 @@ def esd(timeseries, max_anomalies=10, alpha=0.05):
         list int: The indices of the anomalies in the timeseries.
 
     """
-    ts = np.copy(np.array(timeseries))
+    ts = list(zip(timeseries, range(len(timeseries))))
     test_statistics = []
     total_anomalies = -1
     for curr in range(max_anomalies):
-        test_idx, test_val = calculate_test_statistic(ts)
-        critical_value     = calculate_critical_value(ts, alpha)
+
+        test_val, test_idx = calculate_test_statistic(ts)
+
+        critical_value = calculate_critical_value(ts, alpha)
         if test_val > critical_value:
             total_anomalies = curr
-        test_statistics.append(test_idx)
-        ts = np.delete(ts, test_idx)
+        test_statistics.append(ts[test_idx][1])
+        del ts[test_idx]
     anomalous_indices = test_statistics[:total_anomalies + 1]
-    return anomalous_indices
-
-
+    return [x for x in anomalous_indices]
